6f279d36201b76a3310768fa2a4f6ef3d48470db
edumag <edu@lesolivex.com>
2020-05-10 17:33:48 +0200

    Correcciones

--- autostart.sh
+++ autostart.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-egrep -rh '^Exec' ~/.config/autostart | while read -r line ; do 
+egrep -rh '^Exec' $HOME/.config/autostart | while read -r line ; do 
    cmd=`echo $line | cut -d= -f2-`
    # echo $cmd
    eval "$cmd" >/dev/null &
 done
 
-if [ -d ~/.config/autostart-scripts ] ; then
-    cd ~/.config/autostart-scripts
+if [ -d $HOME/.config/autostart-scripts ] ; then
+    cd $HOME/.config/autostart-scripts
     for c in "*" ; do
         $c
     done
