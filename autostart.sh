27d4ead0675b9bec1964355965618117268393d3
edumag <edu@lesolivex.com>
2020-11-18 19:31:56 +0100

    BugFix: autostart

--- autostart.sh
+++ autostart.sh
@@ -2,13 +2,14 @@
 egrep -rh '^Exec' $HOME/.config/autostart | while read -r line ; do 
    cmd=`echo $line | cut -d= -f2-`
    # echo $cmd
-   eval "$cmd" >/dev/null &
+   eval "$cmd" & # >/dev/null &
 done
 
 if [ -d $HOME/.config/autostart-scripts ] ; then
     cd $HOME/.config/autostart-scripts
-    for c in "./*" ; do
-        $c
+    for launch in * ; do
+      # echo "$launch"
+      ./$launch &
     done
     cd -
 fi
