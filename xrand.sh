e2995b8ab936e95944ea9b73f782ebd4dc3bba49
edumag <edu@lesolivex.com>
2024-04-27 20:12:39 +0200

    xrand.sh 1 o 2 pantallas y clipit

--- xrand.sh
+++ xrand.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-xrandr --output eDP --mode 1680x1050 --pos 0x900 --rotate normal --output HDMI-A-0 --primary --mode 1600x900 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
+
+if [ "$1" == "2" ] ; then
+  # Dos monitores.
+  xrandr --output eDP --mode 1680x1050 --pos 0x900 --rotate normal --output HDMI-A-0 --primary --mode 1600x900 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
+else
+  # Un monitor.
+  xrandr --output eDP --mode 1680x1050
+fi
+
