86c6dcf0412a6cf8ccd64776da4a168bc56c8888
edumag <edu@lesolivex.com>
2024-05-01 10:57:28 +0200

    Gestionamos brillo desde xrand

--- brightness.sh
+++ brightness.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# ACTUAL=`xrandr --current --verbose | grep Brightness | cut -d' ' -f2`
-ACTUAL=`sudo cat /sys/class/backlight/intel_backlight/brightness`
+# Monitor
+monitores=`xrandr --listmonitors | cut -d ' ' -f6 `
 
+# ACTUAL=`xrandr --current --verbose | grep Brightness | cut -d' ' -f2`
+# ACTUAL=`sudo cat /sys/class/backlight/intel_backlight/brightness`
+ACTUAL=`xrandr --current --verbose | grep Brightness | head -n 1 | cut -d' ' -f2`
+
+monitores=`xrandr --listmonitors | cut -d ' ' -f6 `
 if [ -z $1 ] ; then
     echo
     echo "uso: `basename $0` [+|-]"
@@ -10,18 +15,24 @@ if [ -z $1 ] ; then
 fi
 
 if [ "$1" == "+" ] ; then
-    NUEVO=`echo $ACTUAL + 10 | bc`
+    NUEVO=`echo $ACTUAL + 0.1 | bc`
 else
-    NUEVO=`echo $ACTUAL - 10 | bc`
+    NUEVO=`echo $ACTUAL - 0.1 | bc`
 fi
 
-#xrandr --output LVDS-1 --brightness $NUEVO
-echo $NUEVO | sudo tee /sys/class/backlight/intel_backlight/brightness
+echo "Nuevo: $NUEVO"
+# exit
 
+for m in $monitores ; do
+  xrandr --output $m --brightness $NUEVO
+done
+# echo $NUEVO | sudo tee /sys/class/backlight/intel_backlight/brightness
+
+echo "Brightness: $NUEVO"
 
 if [ $? == 0 ] ; then
-    killall dunst
-    notify-send  "Brightness: $NUEVO" 
+    # killall dunst
+    # notify-send  "Brightness: $NUEVO"
     exit
 fi
 
