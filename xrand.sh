9588533b11d8a309134b483de045a88668e409dd
edumag <edu@lesolivex.com>
2024-10-01 08:52:29 +0200

    Aseguramos script en bash

--- xrand.sh
+++ xrand.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$1" == "2" ] ; then
+if [[ "$1" -gt "2" ]] ; then
   # Dos monitores.
   # ACER
   # xrandr --output eDP --mode 1440x900 --pos 80x900 --rotate normal --output HDMI-A-0 --primary --mode 1600x900 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
