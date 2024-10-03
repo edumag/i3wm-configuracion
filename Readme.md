2b85eb201ab7c1aaee3fc6329d38797ecd727b5d
edumag <edu@lesolivex.com>
2024-10-03 08:27:53 +0200

    i3exit.sh

--- Readme.md
+++ Readme.md
@@ -6,11 +6,11 @@
 
 ### debian o derivadas.
 
-    sudo apt-get install i3 dunst compton terminator feh rofi system-config-printer gdebi-core network-manager-gnome arandr keychain pasystray parcellite xfce4-power-manager
+    sudo apt-get install i3 dunst compton terminator feh rofi system-config-printer gdebi-core network-manager-gnome arandr keychain pasystray ksnip parcellite xfce4-power-manager
 
 ### arch o derivadas
 
-    sudo pacman -S i3 i3-gaps dunst terminator feh rofi arandr network-manager-applet keychain sysstat pasystray ksnip parcellite xfce4-power-manager
+    sudo pacman -S i3 i3-gaps dunst terminator feh rofi arandr network-manager-applet keychain sysstat pasystray ksnip parcellite xfce4-power-manager ksnip
 
 ### Bajamos configuración.
 
--- xrand.sh
+++ xrand.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
 if [[ "$1" -gt "2" ]] ; then
-  # Dos monitores.
+  echo "Dos monitores."
   # ACER
   # xrandr --output eDP --mode 1440x900 --pos 80x900 --rotate normal --output HDMI-A-0 --primary --mode 1600x900 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
   # Philips
-  xrandr \
-      --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal \
-      --output eDP --primary --mode 1440x900 --pos 1920x334 --rotate normal
+  # xrandr \
+  #     --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal \
+  #     --output eDP --primary --mode 1440x900 --pos 1920x334 --rotate normal
+  # xrandr --output eDP --primary --mode 1440x900 --pos 1920x180 --rotate normal \
+  #     --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal \
+  #     --output DisplayPort-0 --off --output DisplayPort-1 --off \
+  #     --output DisplayPort-2 --off --output DisplayPort-3 --off \
+  #     --output DisplayPort-4 --off --output DisplayPort-5 --off
+  xrandr --output eDP --primary --mode 1440x900 --pos 1920x180 --rotate normal \
+      --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal
 
-elif [ "$1" == "3" ] ; then
+elif [ "$1" -gt "3" ] ; then
+  echo "Apagamos pantalla portatil."
 
   xrandr --output eDP --off \
       --output HDMI-A-0  --primary --mode 1920x1080 --pos 0x0 --rotate normal
 
 
 else
-  # Un monitor.
+  echo "Un monitor."
   # xrandr --output eDP --mode 1680x1050
   xrandr --output eDP --mode 1440x900 --pos 80x900 --rotate normal
 fi
