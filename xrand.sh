ddd07cd4dbdffabff95b3fd5b6b9861d348adfde
edumag <edu@lesolivex.com>
2024-10-04 15:36:14 +0200

    Corregimos script xrand.sh

--- xrand.sh
+++ xrand.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$1" -gt "2" ]] ; then
+if [ $1 = "2" ] ; then
   echo "Dos monitores."
   # ACER
   # xrandr --output eDP --mode 1440x900 --pos 80x900 --rotate normal --output HDMI-A-0 --primary --mode 1600x900 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
@@ -16,7 +16,7 @@ if [[ "$1" -gt "2" ]] ; then
   xrandr --output eDP --primary --mode 1440x900 --pos 1920x180 --rotate normal \
       --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal
 
-elif [ "$1" -gt "3" ] ; then
+elif [ $1 = "3" ] ; then
   echo "Apagamos pantalla portatil."
 
   xrandr --output eDP --off \
