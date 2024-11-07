208929531c964422a1931ece36cf75a295cf54e8
edumag <edu@lesolivex.com>
2024-11-07 18:43:01 +0100

    Monitor pequeño menos resolución

--- scripts/xrand.sh
+++ scripts/xrand.sh
@@ -2,18 +2,10 @@
 
 if [ $1 = "2" ] ; then
   echo "Dos monitores."
-  # ACER
-  # xrandr --output eDP --mode 1440x900 --pos 80x900 --rotate normal --output HDMI-A-0 --primary --mode 1600x900 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
-  # Philips
-  # xrandr \
-  #     --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal \
-  #     --output eDP --primary --mode 1440x900 --pos 1920x334 --rotate normal
   # xrandr --output eDP --primary --mode 1440x900 --pos 1920x180 --rotate normal \
-  #     --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal \
-  #     --output DisplayPort-0 --off --output DisplayPort-1 --off \
-  #     --output DisplayPort-2 --off --output DisplayPort-3 --off \
-  #     --output DisplayPort-4 --off --output DisplayPort-5 --off
-  xrandr --output eDP --primary --mode 1440x900 --pos 1920x180 --rotate normal \
+  #     --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal
+  # Monitor pequeño menos resolución.
+  xrandr --output eDP --primary --mode 1280x800 --pos 1920x373 --rotate normal \
       --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal
 
 elif [ $1 = "3" ] ; then
