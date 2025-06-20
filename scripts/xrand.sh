53fb2b6c072286d15334a0848311e155a70a505c
edumag <edu@lesolivex.com>
2025-06-20 15:24:03 +0200

    Configuración monitores

--- scripts/xrand.sh
+++ scripts/xrand.sh
@@ -4,18 +4,15 @@ OPTION=${1:-"0"}
 
 if [ "$OPTION" = "2" ] ; then
   echo "Dos monitores."
-  # xrandr --output eDP --primary --mode 1440x900 --pos 1920x180 --rotate normal \
-  #     --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal
-  # Monitor pequeño menos resolución.
   xrandr --output eDP --primary --mode 1280x800 --pos 1920x373 --rotate normal \
       --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal
 
 elif [ "$OPTION" = "3" ] ; then
-  echo "Apagamos pantalla portatil."
+  echo "Apagamos pantalla portátil."
 
   xrandr --output eDP --off
   xrandr \
-      --output DisplayPort-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+      --output DisplayPort-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
   xrandr \
       --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 
@@ -23,17 +20,13 @@ elif [ "$OPTION" = "4" ] ; then
   echo "Monitor externo en hub."
   xrandr \
       --output eDP --primary --mode 1280x800 --pos 1920x206 --rotate normal \
-      --output HDMI-A-0 --off \
-      --output DisplayPort-0 --off \
-      --output DisplayPort-1 --mode 1920x1080 --pos 0x0 --rotate normal \
-      --output DisplayPort-2 --off \
-      --output DisplayPort-3 --off \
-      --output DisplayPort-4 --off \
-      --output DisplayPort-5 --off
+      --output DisplayPort-1 --mode 1920x1080 --pos 0x0 --rotate normal
 
 else
   echo "Un monitor."
   # xrandr --output eDP --mode 1680x1050
-  xrandr --output eDP --mode 1440x900 --pos 80x900 --rotate normal
+  xrandr \
+      --output eDP --primary --mode 1440x900 --pos 80x900 --rotate normal \
+      --output HDMI-A-0 --off
 fi
 
