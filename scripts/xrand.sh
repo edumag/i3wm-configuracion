d22c16720d7b671d83b75137997c79edce60c3a1
edumag <edu@lesolivex.com>
2025-07-16 09:21:00 +0200

    Here's a concise commit message based on the diff:
    ```
    Reorganize autostart and monitor config, improve xrand.sh script
    
    - Moved autostart and monitor scripts to end of i3 config
    - Updated xrand.sh with SECOND_MONITOR variable for consistency
    - Adjusted monitor positioning commands in xrand.sh
    ```

--- scripts/xrand.sh
+++ scripts/xrand.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+# SECOND_MONITOR="HDMI-A-0"
+SECOND_MONITOR="DisplayPort-1"
+
 OPTION=${1:-"0"}
 
 if [ "$OPTION" = "2" ] ; then
   echo "Dos monitores."
-  xrandr --output eDP --primary --mode 1280x800 --pos 1920x373 --rotate normal \
-      --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal
+  xrandr \
+      --output $SECOND_MONITOR --mode 1920x1080 --pos 0x0 --rotate normal \
+      --output eDP --primary --mode 1280x800 --pos 1920x373 --rotate normal
 
 elif [ "$OPTION" = "3" ] ; then
   echo "Apagamos pantalla portátil."
 
   xrandr --output eDP --off
   xrandr \
-      --output DisplayPort-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
-  xrandr \
-      --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+      --output $SECOND_MONITOR --primary --mode 1920x1080 --pos 0x0 --rotate normal
 
 elif [ "$OPTION" = "4" ] ; then
   echo "Monitor externo en hub."
@@ -27,6 +29,6 @@ else
   # xrandr --output eDP --mode 1680x1050
   xrandr \
       --output eDP --primary --mode 1440x900 --pos 80x900 --rotate normal \
-      --output HDMI-A-0 --off
+      --output $SECOND_MONITOR --off
 fi
 
